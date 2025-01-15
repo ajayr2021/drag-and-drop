e27a2e2bc288b7f54ae49819aaebfa2eef1c5d2c
Ajay Rathod <ajayr@vedlogic.com>
2025-01-15 11:27:54 +0530

    DND LOGIC ADDED

--- src/Components/Common/TagCard/index.tsx
+++ src/Components/Common/TagCard/index.tsx
@@ -1,11 +1,20 @@
-import { tagData } from "src/Types";
+import { tagData, TagNotes } from "src/Types";
 import { TagCardWrapper } from "./TagCard.style";
 import { Box, Card, CardContent, Typography } from "@mui/material";
 import { useTagNotes } from "src/Context/TagNotes/TagNotesContext";
+import { DragEvent } from "react";
 
 const TagCard: React.FC<{ tag: tagData }> = ({ tag }) => {
   const { getNotesByTagId } = useTagNotes();
   const notes = getNotesByTagId(tag.id);
+
+  const handleDragStart = (
+    event: DragEvent<HTMLDivElement>,
+    note: TagNotes,
+    tagId: number
+  ) => {
+    event.dataTransfer.setData("dnd", JSON.stringify({ note, tagId }));
+  };
 
   return (
     <TagCardWrapper variant="elevation">
@@ -15,7 +24,13 @@ const TagCard: React.FC<{ tag: tagData }> = ({ tag }) => {
         </Typography>
         <Box className="notes-list">
           {notes.map((item) => (
-            <Card key={item.id} variant="outlined" className="note-card">
+            <Card
+              key={item.id}
+              variant="outlined"
+              className="note-card"
+              draggable
+              onDragStart={(event) => handleDragStart(event, item, tag.id)}
+            >
               <CardContent>
                 <Typography>{item.note}</Typography>
               </CardContent>
--- src/Components/Pages/TagNotes/index.tsx
+++ src/Components/Pages/TagNotes/index.tsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { DragEvent, useState } from "react";
 import { useTags } from "src/Context/Tags/TagContext";
 import { TagNotesWrapper } from "./TagNotes.style";
 import { Box, Button } from "@mui/material";
 import TagCard from "src/Components/Common/TagCard";
 import AddIcon from "@mui/icons-material/Add";
 import AddTagNotesModel from "./Models/AddTagNotesModel";
+import { useTagNotes } from "src/Context/TagNotes/TagNotesContext";
+import { TagNotes as TagNotesType } from "src/Types";
 
 const TagNotes = () => {
   const { tags } = useTags();
+  const { addNotes, deleteNotes } = useTagNotes();
 
   const [dialogState, setDialogState] = useState({
     open: false,
@@ -19,6 +22,21 @@ const TagNotes = () => {
 
   const handleCloseDialog = () => {
     setDialogState({ open: false });
+  };
+
+  const handleDrop = (event: DragEvent<HTMLDivElement>, dropTagId: number) => {
+    event.preventDefault();
+    const {
+      note: item,
+      tagId: sourceTagId,
+    }: { note: TagNotesType; tagId: number } = JSON.parse(
+      event.dataTransfer.getData("dnd")
+    );
+
+    if (sourceTagId === dropTagId) return;
+
+    deleteNotes(item.id);
+    addNotes(item.note, dropTagId);
   };
 
   return (
@@ -34,7 +52,15 @@ const TagNotes = () => {
       </Box>
       <Box className="note-list">
         {!!tags.length ? (
-          tags.map((tag) => <TagCard key={tag.id} tag={tag} />)
+          tags.map((tag) => (
+            <Box
+              key={tag.id}
+              onDrop={(e) => handleDrop(e, tag.id)}
+              onDragOver={(event) => event.preventDefault()}
+            >
+              <TagCard tag={tag} />
+            </Box>
+          ))
         ) : (
           <Box className="tag-no-found">No Tag Found</Box>
         )}
